631a7b5b4f5bfef1eb12406db46d139c0ac8f61b
cosimosacco <cosimosacco@gmail.com>
2011-09-17 15:40:53 +0000

    Aggiunta la classe Task.

---
+++ Model/Task/Task.h
@@ -0,0 +1,46 @@
+/**
+ * @file Task.h
+ * @author Cosimo Sacco <cosimosacco@gmail.com>
+ *
+ * @section LICENSE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+class Task
+{
+    private:
+        const int arrivalTime;
+        const int computationTime;
+        const int relativeDeadline;
+        const int absoluteDeadline;
+        int elapsedTime;
+        int instantaneousExceedingTime;
+    protected:
+    public:
+    Task
+    (
+        const int arrivalTime,
+        const int computationTime,
+        const int relativeDeadline
+    );
+    int getArrivalTime();
+    int getComputationTime();
+    int getRelativeDeadline();
+    int getAbsoluteDeadline();
+    int getElapsedTime();
+    int getInstantaneousExceedingTime();
+    bool deadlineMiss();
+};
