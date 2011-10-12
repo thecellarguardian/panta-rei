05c94a245ced9da0a9f3d8517e67c634fcae4b04
cosimosacco@gmail.com <cosimosacco@gmail.com@be4d08ec-c103-6867-d316-6a97f826d34d>
2011-10-12 19:46:35 +0000

    At p. 9 in the "Rate Monotonic vs. EDF: the Judgement Day" there' s an exemple which fails on this simulator. It ir reported in the provaButtazzo.cpp test, some major changes are coming. The main problem is: when a deadline miss occurs, the execution related to that period could be delayed by several periods. In the current version, the executions related to those periods are totally ignored, while they should be kept in consideration (for example, if no other task has the required priority and there are past executions starving, those should be scheduled in a fifo way). The problem is non trivial: a possible solution is to add a state variable to the PeriodicTask (or maybe in Task) which counts the starving executions, and expand the decision tree properly.

--- trunk/test/provaButtazzo.cpp
+++ trunk/test/provaButtazzo.cpp
@@ -10,12 +10,10 @@ int main()
 	boost::shared_ptr<SystemQueuesManager> systemQueues(new SystemQueuesManager());
 	boost::shared_ptr<Activator> activator(new Activator(systemQueues, &timer));
 	boost::shared_ptr<Scheduler> scheduler(new DeadlineMonotonic(true, systemQueues, &timer, activator));
-	boost::shared_ptr<Task> p1(new PeriodicTask(1, 0, 4, 5, 6, &timer));
-	boost::shared_ptr<Task> p2(new PeriodicTask(2, 3, 2, 4, 4, &timer));
-	boost::shared_ptr<Task> p3(new PeriodicTask(3, 4, 5, 7, 8, &timer));
+	boost::shared_ptr<Task> p1(new PeriodicTask(1, 0, 2, 5, 5, &timer));
+	boost::shared_ptr<Task> p2(new PeriodicTask(2, 0, 4, 7, 7, &timer));
 	activator->registerForActivation(p1);
 	activator->registerForActivation(p2);
-	activator->registerForActivation(p3);
 	timer.start();
 	return 0;
 }
