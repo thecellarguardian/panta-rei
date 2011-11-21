e9c444c3b3d9ef29259d8fa6ad6904df6bcf069a
cosimosacco@gmail.com <cosimosacco@gmail.com@be4d08ec-c103-6867-d316-6a97f826d34d>
2011-11-21 11:18:16 +0000

    Functioning command interpreter done! Now it has to be split into proper classes and a suitable grammar must be defined.

--- trunk/src/Controller/spiritTest.cpp
+++ trunk/src/Controller/spiritTest.cpp
@@ -2,18 +2,104 @@
 #include <boost/spirit/include/qi.hpp>
 #include <string>
 #include <iostream>
+#include <exception>
 #include <boost/spirit/include/phoenix_core.hpp>
 #include <boost/spirit/include/phoenix_operator.hpp>
 
-class Interpreter
-{
-    
-};
-
 void f()
 {
-    std::cout << "ciao" << std::endl;
+    std::cout << "f()" << std::endl;
 }
+
+void g()
+{
+    std::cout << "g()" << std::endl;
+}
+
+
+class ExitException {};
+class InvalidCommandException : public std::exception
+{
+	private:
+		std::string message;
+	public:
+		InvalidCommandException(std::string invalidCommand)
+		{
+			message = "Invalid command: ";
+			message.append("\"").append(invalidCommand).append("\"");
+		}
+		~InvalidCommandException() throw(){}
+		const char* what() const throw()
+		{
+			return message.c_str();
+		}
+};
+
+class CommandInterpreter
+{
+	private:
+		std::string welcomeMessage;
+    public:
+    	CommandInterpreter(std::string welcomeMessageToSet)
+    		: welcomeMessage(welcomeMessageToSet)
+    	{}
+    	void prompt()
+    	{
+    		std::cout << welcomeMessage << std::endl;
+    		std::string command;
+    		while(true)
+    		{
+    			std::cout << ">> ";
+    			std::cin >> command;
+    			try
+    			{
+    				execute(command);
+    			}
+    			catch(ExitException& exit)
+    			{
+    				break;
+    			}
+    			catch(std::exception& caughtException)
+    			{
+    				std::cout << caughtException.what() << std::endl;
+    			}
+    		}
+    	}
+    	virtual void execute(std::string command) = 0;
+};
+
+class PantaReiCommandInterpreter : public CommandInterpreter
+{
+	public:
+		PantaReiCommandInterpreter() : CommandInterpreter("Welcome!"){}
+		void execute(std::string command)
+    	{
+    		if(command.compare("exit") == 0)
+    		{
+    			ExitException e;
+    			throw e;
+    		}
+    		bool validCommand =
+    			boost::spirit::qi::phrase_parse
+    				(
+    					command.begin(),
+    					command.end(),
+            			//  Begin grammar
+            			(
+                    	boost::spirit::qi::string("f")[f] |
+                    	boost::spirit::qi::string("g")[g]
+            			),
+            			//  End grammar
+            			boost::spirit::ascii::space
+            		);
+            if(!validCommand)
+            {
+    			InvalidCommandException invalidCommand(command);
+    			throw invalidCommand;
+    		}
+    	}
+
+};
 
 template <typename Iterator>
     int parse_integer(Iterator first, Iterator last)
@@ -45,7 +131,8 @@ int main()
 {
     std::string s;
     std::cin >> s;
-    
     std::cout << parse_integer(s.begin(), s.end()) << std::endl;
+    PantaReiCommandInterpreter C;
+    C.prompt();
     return 0;
 }
