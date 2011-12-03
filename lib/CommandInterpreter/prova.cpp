61549638836856fb8bf702b4cafff48f7b20c6d9
cosimosacco <cosimosacco@gmail.com>
2011-12-03 18:17:55 +0000

    lib/CommandInterpreter/provaskip.cpp contains a working space skipping parser which has to be modified with the PantaReiSyntax in lib/CommandInterpreter/prova.cpp

--- lib/CommandInterpreter/prova.cpp
+++ lib/CommandInterpreter/prova.cpp
@@ -1,6 +1,5 @@
+#include <boost/bind.hpp>
 #include "CommandInterpreter.h"
-#include <string>
-#include <iostream>
 
 void f()
 {
@@ -12,32 +11,118 @@ void g()
     std::cout << "g" << std::endl;
 }
 
-class PantaReiGrammar :
+void h()
+{
+    std::cout << "h" << std::endl;
+}
+
+void parametri(unsigned int a, unsigned int b, unsigned int c, unsigned int d)
+{
+    std::cout << a << b << c << d << std::endl;
+}
+
+class PantaReiSyntax :
     public boost::spirit::qi::grammar
         <std::string::const_iterator, boost::spirit::ascii::space_type>
 {
     public:
-    PantaReiGrammar() : PantaReiGrammar::base_type(start)
+    PantaReiSyntax() : PantaReiSyntax::base_type(command)
     {
-        command =
-                //createStatement                          |
-                //setStatement                             |
-                //str_p("simulate")[f] |
-                //str_p("help")[f]     |
-                boost::spirit::qi::double_[f]
+        command %=
+                createStatement                    |
+                setStatement                       |
+                boost::spirit::qi::lit("simulate")[f] |
+                boost::spirit::qi::lit("help")[g]     |
+                boost::spirit::qi::lit("quit")[h]
             ;
-        start =
-            command |
-            boost::spirit::qi::lit("ciao")[g]
-            ;
+        createStatement %=
+            boost::spirit::qi::lit("create") >>
+            objectStatement
+        ;
+        setStatement =
+            boost::spirit::qi::lit("set") >>
+            propertyStatement
+        ;
+        objectStatement %=
+            (
+                boost::spirit::qi::lit("periodic")/* >>
+                boost::spirit::qi::uint_ >>
+                boost::spirit::qi::uint_ >>
+                boost::spirit::qi::uint_ >>
+                boost::spirit::qi::uint_*/
+            )
+            [
+                //boost::bind(&parametri, _2, _3, _4, _5)
+                f
+            ]
+        ;
+        propertyStatement =
+            (
+                boost::spirit::qi::lit("simulation length") >>
+                boost::spirit::qi::uint_
+            )
+            /*[
+                boost::bind(&parametri, _2, 0, 0, 0)
+            ]*/
+            |
+            (
+                boost::spirit::qi::lit("scheduler") >>
+                schedulingAlgorithm
+            )
+        ;
+        schedulingAlgorithm =
+            boost::spirit::qi::lit("RM")  |
+            boost::spirit::qi::lit("DM")  |
+            boost::spirit::qi::lit("EDF")
+        ;
     }
-    boost::spirit::qi::rule<std::string::const_iterator, boost::spirit::ascii::space_type> start;
-    boost::spirit::qi::rule<std::string::const_iterator, boost::spirit::ascii::space_type> command;
+    boost::spirit::qi::rule
+        <
+            std::string::const_iterator,
+            boost::spirit::ascii::space_type
+        >
+        command
+    ;
+    boost::spirit::qi::rule
+        <
+            std::string::const_iterator,
+            boost::spirit::ascii::space_type
+        >
+        createStatement
+    ;
+    boost::spirit::qi::rule
+        <
+            std::string::const_iterator,
+            boost::spirit::ascii::space_type
+        >
+        setStatement
+    ;
+    boost::spirit::qi::rule
+        <
+            std::string::const_iterator,
+            boost::spirit::ascii::space_type
+        >
+        objectStatement
+    ;
+    boost::spirit::qi::rule
+        <
+            std::string::const_iterator,
+            boost::spirit::ascii::space_type
+        >
+        propertyStatement
+    ;
+    boost::spirit::qi::rule
+        <
+            std::string::const_iterator,
+            boost::spirit::ascii::space_type
+        >
+        schedulingAlgorithm
+    ;
 };
 
 int main()
 {
-    CommandInterpreter<PantaReiGrammar> c("<<>>");
+    CommandInterpreter<PantaReiSyntax> c("<<>>");
     c.prompt();
 
     return 0;
