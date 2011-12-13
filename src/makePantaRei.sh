c3e79d4b247bc30974d2ce0e33871d5956cd2a89
cosimosacco <cosimosacco@gmail.com>
2011-12-13 13:29:42 +0000

    Ok, making the main executable... script and log have to be propagated through all the code.

---
+++ src/makePantaRei.sh
@@ -0,0 +1,11 @@
+g++ \
+Model/Activator/*.cpp \
+Model/Scheduler/*.cpp \
+Model/SystemQueuesManager/SystemQueuesManager.cpp \
+Model/Task/*.cpp \
+Model/Task/PeriodicTask/*.cpp \
+Model/Timer/Timer.cpp \
+Model/SchedulingSimulation/SchedulingSimulation.cpp \
+../lib/DesignPatterns/Observer/Subject.cpp \
+PantaRei.cpp \
+-lboost_thread -lboost_program_options -lpthread -g -p
